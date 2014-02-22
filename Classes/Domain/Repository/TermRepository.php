7255bae71932ffb8ea398f226c1406ba29f61642
Frank Nägler <franae@blade.fritz.box>
2014-02-23 00:22:43 +0100

    added find helper to the repository

--- Classes/Domain/Repository/TermRepository.php
+++ Classes/Domain/Repository/TermRepository.php
@@ -24,10 +24,69 @@ namespace Dpn\DpnGlossary\Domain\Repository;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use Dpn\DpnGlossary\Domain\Model\Term;
+use TYPO3\CMS\Core\Utility\StringUtility;
+use TYPO3\CMS\Documentation\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 
 /**
  *
  * @package dpn_glossary
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
-class TermRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {}
+class TermRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+
+	/**
+	 * find all terms ordered by name
+	 *
+	 * @param string $orderField
+	 *
+	 * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
+	 */
+	public function findAllOrderedByName($orderField = 'name') {
+		$query = $this->createQuery();
+		$query->setOrderings(array($orderField => QueryInterface::ORDER_ASCENDING));
+		return $query->execute();
+	}
+
+	/**
+	 * find all terms ordered by name and grouped by first character
+	 *
+	 * @return array
+	 */
+	public function findAllGroupedByFirstCharacter() {
+		$terms          = $this->findAllOrderedByName();
+		$numbers        = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+		$normalChars    = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+		$sortedTerms    = array();
+		foreach ($terms as $term) {
+			/** @var Term $term */
+			mb_internal_encoding("UTF-8");
+			$firstCharacter = mb_strtolower(mb_substr($term->getName(), 0, 1));
+			if (in_array($firstCharacter, $numbers)) {
+				$firstCharacter = '0-9';
+			} else if (!in_array($firstCharacter, $normalChars)) {
+				switch ($firstCharacter) {
+					case 'ä':
+						$firstCharacter = 'a';
+					break;
+					case 'ö':
+						$firstCharacter = 'o';
+					break;
+					case 'ü':
+						$firstCharacter = 'u';
+					break;
+					default:
+						$firstCharacter = '_';
+					break;
+				}
+			}
+			$firstCharacter = strtoupper($firstCharacter);
+			if (!isset($sortedTerms[$firstCharacter])) {
+				$sortedTerms[$firstCharacter] = array();
+			}
+			$sortedTerms[$firstCharacter][] = $term;
+		}
+		return $sortedTerms;
+	}
+}
