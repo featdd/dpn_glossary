8dc142d44b2fe31cf89175ec3b9cca52019e8390
Daniel Dorndorf <dorndorf@dreipunktnull.com>
2014-10-13 11:24:53 +0200

    [TASK] Template and asset optimization

--- Resources/Public/js/accordion.js
+++ Resources/Public/js/accordion.js
@@ -1,17 +1,13 @@
-$(window).load(function() {
-
-	$('.term-details .description h3').on("click", function(e) {
-		e.preventDefault();
-		if (!$(this).parents(".description").hasClass("open")) {
-			$(".term-details .description.open .text").slideUp("slow");
-			$(".term-details .description.open").removeClass("open");
-
-			$(this).parents(".description").addClass("open");
-			$(this).parents(".description").find(".text").slideDown("slow");
-		} else {
-			$(".term-details .description.open .text").slideUp("slow");
-			$(".term-details .description.open").removeClass("open");
-		}
-	});
-
+jQuery(function ($) {
+  $('.term-details .description h3').on('click', function () {
+    if (!$(this).parents('.description').hasClass('open')) {
+      $('.term-details .description.open .text').slideUp('slow');
+      $('.term-details .description.open').removeClass('open');
+      $(this).parents('.description').addClass('open');
+      $(this).parents('.description').find('.text').slideDown('slow');
+    } else {
+      $('.term-details .description.open .text').slideUp('slow');
+      $('.term-details .description.open').removeClass('open');
+    }
+  });
 });
