b899652584fd8bfb039321fce6252c66d0683d57
Daniel Dorndorf <dorndorf@featdd.de>
2016-03-22 02:09:40 +0100

    [TASK] Add extension update for images to media field

--- class.ext_update.php
+++ class.ext_update.php
@@ -34,6 +34,18 @@
 class ext_update {
 
 	/**
+	 * @var \TYPO3\CMS\Core\Database\DatabaseConnection
+	 */
+	protected $databaseConnection;
+
+	/**
+	 * @return ext_update
+	 */
+	public function __construct() {
+		$this->databaseConnection = $GLOBALS['TYPO3_DB'];
+	}
+
+	/**
 	 * @return boolean
 	 */
 	public function access() {
@@ -50,45 +62,45 @@ class ext_update {
 		 */
 
 		/** @var mysqli_result $check */
-		$check = $GLOBALS['TYPO3_DB']->sql_query("SHOW COLUMNS FROM tx_dpnglossary_domain_model_term LIKE 'descriptions'");
+		$check = $this->databaseConnection->sql_query('SHOW COLUMNS FROM tx_dpnglossary_domain_model_term LIKE \'descriptions\'');
 
 		if (0 === $check->num_rows) {
-			$GLOBALS['TYPO3_DB']->sql_query("
+			$this->databaseConnection->sql_query('
 				ALTER TABLE tx_dpnglossary_domain_model_term
-				ADD descriptions int(11) unsigned DEFAULT '0'
-			");
+				ADD descriptions int(11) unsigned DEFAULT \'0\'
+			');
 
-			$GLOBALS['TYPO3_DB']->sql_query("
+			$this->databaseConnection->sql_query('
 				CREATE TABLE IF NOT EXISTS tx_dpnglossary_domain_model_description (
 
 					uid int(11) NOT NULL auto_increment,
-					pid int(11) DEFAULT '0' NOT NULL,
+					pid int(11) DEFAULT \'0\' NOT NULL,
 
-					term int(11) unsigned DEFAULT '0' NOT NULL,
-					meaning varchar(255) DEFAULT '' NOT NULL,
+					term int(11) unsigned DEFAULT \'0\' NOT NULL,
+					meaning varchar(255) DEFAULT \'\' NOT NULL,
 					text text NOT NULL,
 
-					tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-					crdate int(11) unsigned DEFAULT '0' NOT NULL,
-					cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-					deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-					hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-					starttime int(11) unsigned DEFAULT '0' NOT NULL,
-					endtime int(11) unsigned DEFAULT '0' NOT NULL,
+					tstamp int(11) unsigned DEFAULT \'0\' NOT NULL,
+					crdate int(11) unsigned DEFAULT \'0\' NOT NULL,
+					cruser_id int(11) unsigned DEFAULT \'0\' NOT NULL,
+					deleted tinyint(4) unsigned DEFAULT \'0\' NOT NULL,
+					hidden tinyint(4) unsigned DEFAULT \'0\' NOT NULL,
+					starttime int(11) unsigned DEFAULT \'0\' NOT NULL,
+					endtime int(11) unsigned DEFAULT \'0\' NOT NULL,
 
-					t3ver_oid int(11) DEFAULT '0' NOT NULL,
-					t3ver_id int(11) DEFAULT '0' NOT NULL,
-					t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-					t3ver_label varchar(255) DEFAULT '' NOT NULL,
-					t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-					t3ver_stage int(11) DEFAULT '0' NOT NULL,
-					t3ver_count int(11) DEFAULT '0' NOT NULL,
-					t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-					t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+					t3ver_oid int(11) DEFAULT \'0\' NOT NULL,
+					t3ver_id int(11) DEFAULT \'0\' NOT NULL,
+					t3ver_wsid int(11) DEFAULT \'0\' NOT NULL,
+					t3ver_label varchar(255) DEFAULT \'\' NOT NULL,
+					t3ver_state tinyint(4) DEFAULT \'0\' NOT NULL,
+					t3ver_stage int(11) DEFAULT \'0\' NOT NULL,
+					t3ver_count int(11) DEFAULT \'0\' NOT NULL,
+					t3ver_tstamp int(11) DEFAULT \'0\' NOT NULL,
+					t3ver_move_id int(11) DEFAULT \'0\' NOT NULL,
 
-					t3_origuid int(11) DEFAULT '0' NOT NULL,
-					sys_language_uid int(11) DEFAULT '0' NOT NULL,
-					l10n_parent int(11) DEFAULT '0' NOT NULL,
+					t3_origuid int(11) DEFAULT \'0\' NOT NULL,
+					sys_language_uid int(11) DEFAULT \'0\' NOT NULL,
+					l10n_parent int(11) DEFAULT \'0\' NOT NULL,
 					l10n_diffsource mediumblob,
 
 					PRIMARY KEY (uid),
@@ -97,25 +109,25 @@ class ext_update {
 					KEY language (l10n_parent,sys_language_uid)
 
 				);
-			");
+			');
 
 
-			$results = $GLOBALS['TYPO3_DB']->exec_SELECTquery('uid,tooltiptext,description', 'tx_dpnglossary_domain_model_term', '', '', '');
+			$results = $this->databaseConnection->exec_SELECTquery('uid,tooltiptext,description', 'tx_dpnglossary_domain_model_term', '', '', '');
 			foreach($results as $result) {
-				$GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_dpnglossary_domain_model_description', array(
+				$this->databaseConnection->exec_INSERTquery('tx_dpnglossary_domain_model_description', array(
 					'term' => $result['uid'],
 					'meaning' => $result['tooltiptext'],
 					'text' => $result['description'],
 				));
 			}
-			$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_dpnglossary_domain_model_term', '', array('descriptions' => 1));
+			$this->databaseConnection->exec_UPDATEquery('tx_dpnglossary_domain_model_term', '', array('descriptions' => 1));
 
-			$GLOBALS['TYPO3_DB']->sql_query("
+			$this->databaseConnection->sql_query('
 				ALTER TABLE tx_dpnglossary_domain_model_term
 				CHANGE description zzz_deleted_description text NOT NULL
-			");
+			');
 
-			return $GLOBALS['TYPO3_DB']->sql_affected_rows() . ' rows have been updated.';
+			return $this->databaseConnection->sql_affected_rows() . ' rows have been updated.';
 		}
 
 		/*
@@ -123,15 +135,26 @@ class ext_update {
 		 */
 
 		/** @var mysqli_result $checkSortingColumn */
-		$checkSortingColumn = $GLOBALS['TYPO3_DB']->sql_query("SHOW COLUMNS FROM tx_dpnglossary_domain_model_description LIKE 'sorting'");
+		$checkSortingColumn = $this->databaseConnection->sql_query("SHOW COLUMNS FROM tx_dpnglossary_domain_model_description LIKE 'sorting'");
 
 		if (0 === $checkSortingColumn->num_rows) {
-			$GLOBALS['TYPO3_DB']->sql_query("
+			$this->databaseConnection->sql_query('
 				ALTER TABLE tx_dpnglossary_domain_model_description
-				ADD sorting int(11) unsigned DEFAULT '0' NOT NULL
-			");
+				ADD sorting int(11) unsigned DEFAULT \'0\' NOT NULL
+			');
 
 			return 'The missing sorting column was added to descriptions';
+		}
+
+		$checkImagesColumn = $this->databaseConnection->sql_query("SHOW COLUMNS FROM tx_dpnglossary_domain_model_description LIKE 'images'");
+
+		if (0 < $checkImagesColumn->num_rows) {
+			$this->databaseConnection->sql_query('
+				ALTER TABLE tx_dpnglossary_domain_model_term
+				CHANGE COLUMN images media int(11) unsigned DEFAULT \'0\';
+			');
+
+			return 'Images field changed to media field';
 		}
 
 		/*
