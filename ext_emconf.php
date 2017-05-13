1a69073a5faac2a70c127841dafc9858544deb2b
Daniel Dorndorf <dorndorf@featdd.de>
2017-05-13 17:29:19 +0200

    [TASK] Increase TYPO3 max dependency to 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -44,9 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version'          => '2.6.13',
     'constraints'      => array(
         'depends'   => array(
-            'extbase' => '6.2.0',
-            'fluid'   => '6.2.0',
-            'typo3'   => '6.2.0-7.6.99',
+            'typo3'   => '7.6.0-8.7.99',
             'php'     => '5.6.0'
         ),
         'conflicts' => array(),
