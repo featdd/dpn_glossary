3322bd3012ce7ecd30c2b88e3762bbf8be5a9c9d
Lex Frolenko <lex.frolenko.stratis@gmail.com>
2018-11-06 13:02:05 +0200

    [TASK] ext:dpn_glossary > DOMDocument > HTML5DOMDocument
    rebase commit 
    added ExtensionManagementUtility
    fixed translationSource

--- Classes/Utility/ParserUtility.php
+++ Classes/Utility/ParserUtility.php
@@ -13,6 +13,7 @@ namespace Featdd\DpnGlossary\Utility;
  ***/
 
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 /**
  * @package DpnGlossary
@@ -135,8 +136,12 @@ class ParserUtility implements SingletonInterface
      */
     public static function domTextReplacer(\DOMText $DOMText, $replacement): void
     {
+        //class HTML5DOMDocument
+        $extPath = ExtensionManagementUtility::extPath('dpn_glossary');
+        require_once($extPath . 'Resources/Private/Libraries/html5DomDocument/autoload.php');
+        
         if (false === empty(trim($replacement))) {
-            $tempDOM = new \DOMDocument();
+            $tempDOM = new \IvoPetkov\HTML5DOMDocument();;
             // use XHTML tag for avoiding UTF-8 encoding problems
             $tempDOM->loadHTML('<?xml encoding="UTF-8">' . '<!DOCTYPE html><html><body><div id="replacement">' . $replacement . '</div></body></html>');
 
